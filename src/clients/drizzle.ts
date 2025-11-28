8bd07e99318379539537dfdf1dc15b68ddc528b7
yutakobaayshidev <hi@yutakobayashi.com>
2025-11-28 21:55:18 +0900

    fix

--- src/clients/drizzle.ts
+++ src/clients/drizzle.ts
@@ -1,10 +1,8 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { neon } from "@neondatabase/serverless";
 import * as schema from "../db/schema";
-import { createDBUrl } from "../utils/db";
+import { drizzle } from "drizzle-orm/neon-http";
 
-const pool = postgres(createDBUrl({}), { max: 1 });
-
-export const db = drizzle(pool, { schema });
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle({ client: sql, schema });
 
 export type Database = typeof db;
