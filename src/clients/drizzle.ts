d2d03a5bbc5b28fc1a41d85c1845e6def39a7db4
yutakobaayshidev <hi@yutakobayashi.com>
2025-11-29 11:28:31 +0900

    fix:db

--- src/clients/drizzle.ts
+++ src/clients/drizzle.ts
@@ -1,10 +1,21 @@
-import { drizzle } from "drizzle-orm/postgres-js";
+import { drizzle as pgDrizzle } from "drizzle-orm/postgres-js";
+import { drizzle as neonDrizzle } from "drizzle-orm/neon-http";
 import postgres from "postgres";
+import { neon, neonConfig } from "@neondatabase/serverless";
+import ws from "ws";
 import * as schema from "../db/schema";
 import { createDBUrl } from "../utils/db";
 
-const pool = postgres(createDBUrl({}), { max: 1 });
+const db = (() => {
+  if (process.env.VERCEL) {
+    neonConfig.webSocketConstructor = ws;
+    const sql = neon(process.env.DATABASE_URL!);
+    return neonDrizzle({ client: sql, schema });
+  } else {
+    const pool = postgres(createDBUrl({}), { max: 1 });
+    return pgDrizzle(pool, { schema });
+  }
+})();
 
-export const db = drizzle(pool, { schema });
-
+export { db };
 export type Database = typeof db;
