491f88195307ea5d24c0b063aed2a8348e456d38
yutakobaayshidev <hi@yutakobayashi.com>
2025-11-29 15:28:52 +0900

    chore: skill

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,11 +1,13 @@
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
 import * as TabsComponents from "fumadocs-ui/components/tabs";
+import * as AccordionComponents from "fumadocs-ui/components/accordion";
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     ...TabsComponents,
+    ...AccordionComponents,
     ...components,
   };
 }
