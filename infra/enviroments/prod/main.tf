b3856e591ec696637f7d19d52acf69d726cf4033
yutakobaayshidev <hi@yutakobayashi.com>
2025-12-11 12:05:46 +0900

    fix: fmt

--- infra/enviroments/prod/main.tf
+++ infra/enviroments/prod/main.tf
@@ -19,11 +19,11 @@ resource "axiom_dataset" "ava_otel_traces" {
 }
 
 module "storage" {
-  source       = "../../modules/cloudflare-r2"
-  project_name = var.project_name
-  environment  = "prod"
-  account_id   = var.cloudflare_account_id
-  r2_location  = "apac"
+  source        = "../../modules/cloudflare-r2"
+  project_name  = var.project_name
+  environment   = "prod"
+  account_id    = var.cloudflare_account_id
+  r2_location   = "apac"
   custom_domain = var.cloudflare_r2_custom_domain
 }
 
